a4f8dfa5a8ba3ec44c2d96a3ff98a2cb5d07f300
Chun Ly <astro.chun@gmail.com>
2020-11-02 14:48:21 -0700

    Set packages in setup #4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='chun_codes',
     version='0.5.0',
-    scripts=find_packages('chun_codes'),
+    packages=['chun_codes'],
     url='https://github.com/astrochun/chun_codes',
     license='MIT License',
     author='Chun Ly',
