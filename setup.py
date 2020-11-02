691b3142abc8ad2bc9e9fef1d163e7165447c69e
Chun Ly <astro.chun@gmail.com>
2020-11-02 16:01:36 -0700

    setup: Define packages
     - Include matplotlib

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='chun_codes',
     version='0.5.0',
-    scripts=find_packages('chun_codes'),
+    packages=['chun_codes'],
     url='https://github.com/astrochun/chun_codes',
     license='MIT License',
     author='Chun Ly',
@@ -14,5 +14,5 @@ setup(
     description='Set of Python 2.7 and 3.xx codes used in astrochun\'s codes',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires = ['numpy', 'astropy']
+    install_requires=['numpy', 'astropy', 'matplotlib']
 )
